679c3623e1794fec06f5b491f1ea605eaa5ff2b0
Богдан <bboybaga13@mail.ru>
2021-10-31 19:03:00 +0300

    My second commit

--- homework.py
+++ homework.py
@@ -1,98 +1,98 @@
 import logging
-
 import os
-
+import time
 from http import HTTPStatus
+
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
-import time
+from telegram.error import TelegramError
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    filename='main.log',
-    filemode='w'
+    filename="main.log",
+    filemode="w",
 )
 logger = logging.getLogger(__name__)
-logger.addHandler(
-    logging.StreamHandler()
-)
+logger.addHandler(logging.StreamHandler())
 
 RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 
 HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена, в ней нашлись ошибки.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена, в ней нашлись ошибки.",
 }
 
 
-def IsRequiredVariables():
+def is_required_variables():
     """Эта функция проверяет обязательные переменнные."""
-    message = 'Program stoped, because required variable is missing'
+    message = "Program stoped, because required variable is missing"
+    stop_program = 0
     if PRACTICUM_TOKEN is None:
-        logger.critical(f'{message} PRACTICUM TOKEN')
+        logger.critical(f"{message} PRACTICUM TOKEN")
+        stop_program = 1
     if TELEGRAM_TOKEN is None:
-        logger.critical(f'{message} TELEGRAM TOKEN')
+        logger.critical(f"{message} TELEGRAM TOKEN")
+        stop_program = 1
     if CHAT_ID is None:
-        logger.critical(f'{message} CHAT_ID')
+        logger.critical(f"{message} CHAT_ID")
+        stop_program = 1
+    return stop_program
 
 
-def TheAnswerIsNot200Error(message):
-    """Функция обрабатывает статус неравный 200."""
-    return message
-
-
-class RequestExceptionError(Exception):
+class RequestException(Exception):
     """Функция для обработки недоступного ENDPOINT."""
+
+
+class TGBotException(Exception):
+    """Класс для обработки пустго API ответа."""
 
 
 def send_message(bot, message):
     """Эта функция отправляет сообщение."""
     try:
-        logger.info(f'Bot sent message: {message}')
+        logger.info(f"Bot sent message: {message}")
         bot.send_message(CHAT_ID, message)
-    except Exception as error:
-        logger.error(f'Error in send message: {error}')
+    except TelegramError as error:
+        logger.error(f"Error in send message: {error}")
 
 
 def get_api_answer(url, current_timestamp):
     """Эта функция делает запрос к API практикума."""
-    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-    payload = {'from_date': current_timestamp}
+    headers = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
+    payload = {"from_date": current_timestamp}
     try:
-        response = requests.get(
-            url, headers=headers, params=payload
-        )
-        if response.status_code != HTTPStatus.OK:
-            message = (
-                f'Endpoint {url} is not available.'
-                f"API response code is {response.status_code}"
-            )
-            logger.error(message)
-            raise TheAnswerIsNot200Error(message)
-        return response.json()
+        response = requests.get(url, headers=headers, params=payload)
     except requests.exceptions.RequestException as request_error:
-        message = (f'API response code is {request_error}')
+        message = f"API response code is {request_error}"
         logger.error(message)
-        raise RequestExceptionError(message)
+        raise RequestException(message)
+    if response.status_code != HTTPStatus.OK:
+        message = (
+            f"Endpoint {url} is not available."
+            f"API response code is {response.status_code}"
+        )
+        logger.error(message)
+        raise TGBotException(message)
+    return response.json()
 
 
 def parse_status(homework):
     """Эта функция оповещает об изменение статуса."""
-    homework_status = homework.get('status')
+    homework_status = homework.get("status")
     verdict = HOMEWORK_STATUSES[homework_status]
     homework_name = homework.get("homework_name")
     if homework_name is None or verdict is None:
-        raise Exception("Invalid results")
+        raise TGBotException("Invalid results")
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -102,15 +102,17 @@ def check_response(response):
         message = "Your API hasn't expected keys"
         logger.error(message)
         return message
-    homeworks = response.get('homeworks')
+    homeworks = response.get("homeworks")
     if homeworks != []:
-        return parse_status(response.get('homeworks')[0])
+        return parse_status(response.get("homeworks")[0])
     return None
 
 
 def main():
     """Функция служебного кода."""
-    IsRequiredVariables()
+    stop_program = is_required_variables()
+    if stop_program == 1:
+        raise TGBotException("Одна из обязательных переменных отсутствует")
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     url = ENDPOINT
@@ -123,13 +125,12 @@ def main():
             time.sleep(RETRY_TIME)
             current_timestamp = int(time.time() - RETRY_TIME)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f"Сбой в работе программы: {error}"
             send_message(bot, message)
             logger.error(message)
             time.sleep(RETRY_TIME)
             current_timestamp = int(time.time() - RETRY_TIME)
-            continue
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
